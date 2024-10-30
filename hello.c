9e558c1c2dc080887df01b78cd7843c25c4a4015
vpc20 <cuevasvp@yahoo.com>
2024-10-30 10:38:47 +0800

    new

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // output a line
+  printf("Hello World!\n");
+
+  // Escape Sequences
+  // new line
+  printf("Hello\nWorld!\n");
+  //tab
+  printf("Hello\tWorld!");
+
+  // compile program
+  // gcc hello.c -o hello
+ 
+}
