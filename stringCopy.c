526ed2361f280eb5e3611b9ebe61df55fb78f164
vpc20 <cuevasvp@yahoo.com>
2024-11-10 07:23:10 +0800

    new

---
+++ stringCopy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void copy(char* dst, char* src) {
+  while (*src != '\0') {
+    *dst = *src;
+    src++;
+    dst++;
+  }
+  *dst = '\0';
+}
+
+int main() {
+  char srcString[] =
+      "We promptly judged antique ivory buckles for the next prize!";
+  char dstString[strlen(srcString)];
+
+  copy(dstString, srcString);
+  printf("%s\n", dstString);
+}
