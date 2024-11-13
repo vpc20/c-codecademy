f7d171e7823c97865e7ee0be287c90bc95c4653d
vpc20 <cuevasvp@yahoo.com>
2024-11-13 17:25:04 +0800

    revision

---
+++ struct_in_func.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Person {
+  char name[40];
+  int age;
+};
+
+struct Person ageOne(struct Person friend1, struct Person* friend2Pointer) {
+  friend1.age += 1;
+  friend2Pointer->age += 1;
+  return friend1;
+}
+
+int main(void) {
+  struct Person myFriend = {"Martin", 57};
+  struct Person myOtherFriend = {"Michelle", 27};
+
+  myFriend = ageOne(myFriend, &myOtherFriend);
+
+  printf("Hello my name is %s and I am %d years old.\n", myFriend.name,
+         myFriend.age);
+  printf("Hello my name is %s and I am %d years old.\n", myOtherFriend.name,
+         myOtherFriend.age);
+}
--- structures.c
+++ structures.c
@@ -10,6 +10,16 @@ int main(void) {
   struct Person person1 = {"Ada", "Lovelace", 28};
   struct Person person2 = {"Marie", "Curie", 44};
 
+  struct Person* person1Pointer = &person1;
+  struct Person* person2Pointer = &person2;
+  (*person1Pointer).age += 1;
+  person2Pointer->age += 10;
+
+  printf("Hello my name is %s and I am %d years old.\n", person1.firstName,
+         person1.age);
+  printf("Hello my name is %s and I am %d years old.\n", person2.firstName,
+         person2.age);
+
   struct Table {
     int length;
     int width;
